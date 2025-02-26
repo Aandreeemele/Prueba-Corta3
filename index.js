e7058f1cede127dbb830abd9b6ac245b7d81d9ed
Aandreeemele <aamelendez@scl.edu.gt>
2025-02-25 20:44:18 -1200

    Version agregar

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 import { iniciarApp } from './componentes/catalogo.js';
 
 document.addEventListener('DOMContentLoaded', iniciarApp);
+document.addEventListener('DOMContentLoaded', () => {
+    // Aquí va todo tu código
+  });
+  
